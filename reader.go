47c495739a781fe65fc260fb9ad94c380300e354
Ben Morgan <neembi@gmail.com>
2015-12-30 09:54:23 +0100

    Adding Reader.Expect method

--- reader.go
+++ reader.go
@@ -37,6 +37,24 @@ func (r *Reader) Backup(t Token) {
 	r.buf = &t
 }
 
+// Expect reads the expected tokens and returns them in a slice.
+// If a token has an unexpected type, it is the last token in the slice
+// and false is returned.
+// The length of the returned slice is the number of tokens read,
+// the capacity is the expected number of tokens to read.
+func (r *Reader) Expect(types ...Type) ([]Token, bool) {
+	tokens, ok := make([]Token, 0, len(types)), true
+	for _, t := range types {
+		arg := r.Next()
+		tokens = append(tokens, arg)
+		if arg.Type != t {
+			ok = false
+			break
+		}
+	}
+	return tokens, ok
+}
+
 func (r *Reader) PosInfo() (name string, line, col int) {
 	return r.lex.Name(), r.lex.LineNumber(), r.lex.ColumnNumber()
 }
